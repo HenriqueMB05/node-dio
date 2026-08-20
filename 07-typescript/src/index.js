25dfe5525cc63e6f575c3060c0161c83263955a8
Matoshi_GD <matosh38@gmail.com>
2026-08-20 11:20:11 -0300

    Revert "aula 07"
    This reverts commit 5f7e8e393bb3e6d846f7a369037fd6e021fd584a.

--- 07-typescript/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function main() {
-    let email = "matosh38@gmail.com";
-    console.log(email);
-}
-;
-main();
--- 07-typescript/src/index.ts
+++
@@ -1,9 +0,0 @@
-function main(){
-  let email:string = process.env.USEREMAIL ;
-  console.log(email);
-};
-
-
-
-main();
-
